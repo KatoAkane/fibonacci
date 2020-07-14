bcd0dcd393d8f99fef87740ffc2fdbc2ce9e23a6
AkanrKato <kato_akane@waku-2.com>
2020-07-14 10:58:41 +0900

    トリボナッチ数列

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 'use strict';
-const memo =new Map();
-memo.set(0, 0);
-memo.set(1, 1);
-function fib(n) {
+// const memo =new Map();
+// memo.set(0, 0);
+// memo.set(1, 1);
+// function fib(n) {
+//     if (memo.has(n)) {
+//         return memo.get(n);
+//     }
+//     const value = fib(n - 1) + fib(n - 2);
+//     memo.set(n, value);
+//     return value;
+// }
+
+// const length = 40;
+// for (let i = 0; i <= length; i++) {
+//     console.log(fib(i));
+// }
+const memo = new Map();
+memo.set(0 ,0);
+memo.set(1, 0);
+memo.set(2, 1);
+function tri(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n - 1) + fib(n - 2);
+    const value =tri(n-3)+ tri(n-2) + tri (n-1);
     memo.set(n, value);
     return value;
 }
 
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
-}
+        console.log(tri(i));
+    }
